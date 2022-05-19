54cbc7848ebf895523724d9c837d33e1070583f1
Dominic Francisco <dominic.m.francisco@gmail.com>
2022-05-19 17:01:57 +0800

    material ui, signin and registration page

---
+++ pages/register.js
@@ -0,0 +1,70 @@
+import { Box,   Button, Paper, TextField, Typography } from "@mui/material";
+import { useRouter } from "next/router";
+
+export default function SignIn() {
+
+    const router = useRouter();
+
+    const style ={
+        linkStyle:{
+            cursor:"pointer",
+            color: "#6134eb"
+        }
+    }
+  return (
+    <Box sx={{
+        width:"100vw", 
+        height: "100vh", 
+        display:"flex",
+        flexDirection:"column", 
+        justifyContent:"Center", 
+        alignItems:"Center"}}>
+            
+            <Box>
+                <Typography variant="h5" >Create New Account</Typography>
+            </Box>
+        <Paper elevation ={3} sx={{
+            padding: "24px", 
+            display:"flex", 
+            flexDirection:"column",
+            marginTop:"12px"}}>
+
+            <TextField label="Email Address" 
+            name="email" 
+            type="email"/>
+
+            <TextField label="Password" 
+            type="password" 
+            name="password" 
+            sx={{marginTop:"12px"}}
+            />
+
+            <TextField label="Confirm Password" 
+            type="password" 
+            name="confirmPassword" 
+            sx={{marginTop:"12px"}}
+            />
+
+            <Button variant="contained" sx={{marginTop:"12px"}}>
+            Register
+            </Button>
+
+            
+            <Box sx={{
+                display:"flex", 
+                justifyContent:"right", 
+                marginTop:"12px"
+                }}>
+
+                <Typography 
+                    variant="subtitle2" 
+                    onClick={() => router.push("/sign-in")} 
+                    sx={style.linkStyle}>
+
+                    Create new account
+                </Typography>
+            </Box>
+        </Paper>
+    </Box>
+  )
+}

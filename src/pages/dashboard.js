4b470c97d02ded2deee87c530564061034dc4f44
DropitupQwert <vjacoballen3020@gmai.com>
2022-06-05 00:02:40 +0800

    my first coommit

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,39 +1,41 @@
-import React from "react";
-import AdminLayout from "../components/AdminLayout";
-import { Box, Grid, Paper, Typography } from "@mui/material";
-import Image from "next/image";
+import React from 'react';
+import AdminLayout from '../components/AdminLayout';
+import { Box, Grid, Paper, Typography } from '@mui/material';
+import Image from 'next/image';
 export default function Dashboard() {
   const style = {
     paper1: {
-      padding: "24px",
+      padding: '24px',
       backgroundColor: (theme) => theme.palette.dashboard.bg1,
     },
     paper2: {
-      padding: "24px",
+      padding: '24px',
       backgroundColor: (theme) => theme.palette.dashboard.bg2,
     },
     paper3: {
-      padding: "24px",
+      padding: '24px',
       backgroundColor: (theme) => theme.palette.dashboard.bg3,
     },
     text1: {
-      fontWeight: "700",
+      fontWeight: '700',
       color: (theme) => theme.palette.text.t2,
     },
   };
 
   return (
-    <Box sx={{ padding: "0 24px" }}>
-      <Box sx={{display:"flex", alignItems:"center"}}>
+    <Box sx={{ padding: '0 24px' }}>
+      <Box sx={{ display: 'flex', alignItems: 'center' }}>
         <Image
           src="/assets/svg/dashboard.svg"
           alt="Dashboard Image"
           width={40}
           height={40}
         />
-        <Typography variant="h1" sx={{marginLeft:"12px"}}>Dashboard</Typography>
+        <Typography variant="h1" sx={{ marginLeft: '12px' }}>
+          Dashboard
+        </Typography>
       </Box>
-      <Grid container spacing={2} sx={{ marginTop: "12px" }}>
+      <Grid container spacing={2} sx={{ marginTop: '12px' }}>
         <Grid item xs={12} sm>
           <Paper sx={style.paper1}>
             <Typography variant="h3" color={(theme) => theme.palette.text.t2}>
